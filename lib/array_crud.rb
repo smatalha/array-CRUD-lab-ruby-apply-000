4791cc14601c3a860a490e6c73b6c51c6f8379ca
Md Abu Talha Mian <sma.talha@yahoo.com>
2020-04-02 17:26:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  
+  array = []
 end
 
 def add_element_to_end_of_array(array, element)
@@ -15,21 +15,21 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop()
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.shift()
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array.index[]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array.index[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array.index[array.length -1]
 end
